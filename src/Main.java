d10ca23e73a86bd1868bb572e72ae2869f19a886
shiftel <marcel.mal3cki@gmail.com>
2023-12-08 03:06:28 +0100

    Yeah

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         int k = 6+9;
-        System.out.println(k);
+        System.out.println(k);}
+
+        public void yeah() {
+            System.out.println("muahhaa");
+        }
     }
-}
